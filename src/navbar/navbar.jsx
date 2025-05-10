78819920627a3bcb5ed0ab350b92eb26bff264f7
Nahuel Gomez <pablonahuelgomez1994@gmail.com>
2025-05-10 00:42:48 -0300

    se acomoda estilo y se arregla el menu de navegacion

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import NavBarItem from "./items";
-import t from "./../resources/translate"
+import t from "./../resources/translate";
+import $ from "jquery"
 
-export default function Navigator({Unload}) {
+export default function Navigator({ Unload }) {
+    useEffect(()=>{
+        const navbarToggler = document.querySelector(".navbar-toggler");
+        const newNavbarToggler = navbarToggler.cloneNode(true);
+        navbarToggler.parentNode.replaceChild(newNavbarToggler, navbarToggler);
+
+        newNavbarToggler.addEventListener("click", function(e) {
+            document.querySelector(".tm-sidebar").classList.toggle("show");
+            e.stopPropagation();
+        });
+    },[]);
     return (
         <div id="tm-sidebar" className="tm-sidebar">
             <nav className="tm-nav">
@@ -14,18 +25,20 @@ export default function Navigator({Unload}) {
                         <h1 className="tm-brand">PG Nahuel</h1>
                     </div>
                     <ul id="tm-main-nav">
-                        <NavBarItem description={t("MAIN_CONTENT")} icon="fa-home" href="#home" onClick={()=>Unload("#home")}/>
-                        <NavBarItem description={t("ARTICLES")} icon="fa-user-friends" href="#articles" onClick={()=>Unload("#articles")}/>
-                        <NavBarItem description={t("CONTACT")} icon="fa-envelope" href="#contact" onClick={()=>Unload("#contact")}/>
+                        <NavBarItem description={t("MAIN_CONTENT")} icon="fa-home" href="#home" onClick={() => Unload("#home")} />
+                        <NavBarItem description={t("ARTICLES")} icon="fa-user-friends" href="#articles" onClick={() => Unload("#articles")} />
+                        <NavBarItem description={t("CONTACT")} icon="fa-envelope" href="#contact" onClick={() => Unload("#contact")} />
                     </ul>
-
-                    <hr />
-
-                    <center>
-                        <ul className="tm-contact-social"><li><a target="_blank" href="https://x.com/NachoPNG" className="tm-social-link"><i className="fab fa-twitter"></i></a></li><li><a target="_blank" href="https://instagram.com/_nacho.png" className="tm-social-link"><i className="fab fa-instagram"></i></a></li><li><a target="_blank" href="https://youtube.com/NahuelGomez94" className="tm-social-link"><i className="fab fa-youtube"></i></a></li><li><a target="_blank" href="https://www.linkedin.com/in/nahuel-g%C3%B3mez-a869617b" className="tm-social-link"><i className="fab fa-linkedin"></i></a></li><li><a target="_blank" href="https://www.ivoox.com/escuchar-audios-nahuel-gomez_al_15792872_1.html" className="tm-social-link"><i className="fa fa-podcast"></i></a></li></ul>
-                    </center>
+                </div>
+                <div className="social-links">
+                    <a target="_blank" href="https://x.com/NachoPNG" className="tm-social-link"><i className="fab fa-twitter"></i></a>
+                    <a target="_blank" href="https://instagram.com/_nacho.png" className="tm-social-link"><i className="fab fa-instagram"></i></a>
+                    <a target="_blank" href="https://youtube.com/NahuelGomez94" className="tm-social-link"><i className="fab fa-youtube"></i></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/nahuel-g%C3%B3mez-a869617b" className="tm-social-link"><i className="fab fa-linkedin"></i></a>
+                    <a target="_blank" href="https://www.ivoox.com/escuchar-audios-nahuel-gomez_al_15792872_1.html" className="tm-social-link"><i className="fa fa-podcast"></i></a>
                 </div>
             </nav>
+
         </div>
 
     )
